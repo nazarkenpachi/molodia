160c8c38cffe17fc1e6f524bb01a7086e16ba9bf
NazarKenpachi <nazar.drozd.ip32@gmail.com>
2024-09-09 23:33:07 +0300

    updated about page

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const gridSize = 169;
-const iconGrid = document.querySelector('.icon-grid')
+const iconGrid = document.querySelector('.icon-grid');
+const gridColumns = 13;
+const gridRows = 6;
+const gridSize = gridColumns * gridRows;
+
 
 for(let i = 0; i< gridSize; i++){
    const icon = document.createElement('i');
@@ -14,5 +17,30 @@ const sr = ScrollReveal ({
    reset: true
 })
 
+// const colors = ['#ffc300', '#f8f8f8'];
+// let currentColorIndex = 0;
+
+// function updateBackgroundColor() {
+//    const scrollPosition = window.scrollY;
+//    const windowHeight = window.innerHeight;
+//    const bodyHeight = document.body.offsetHeight;
+   
+//    const scrollPercentage = scrollPosition / (bodyHeight - windowHeight);
+//    const colorIndex = Math.floor(scrollPercentage * colors.length);
+   
+//    document.body.style.backgroundColor = colors[colorIndex];
+// }
+
+// window.addEventListener('scroll', updateBackgroundColor);
+
+// updateBackgroundColor();
+
+const container = document.querySelector('.container');
+document.querySelector('.slider').addEventListener('input', (e) => {
+  container.style.setProperty('--position', `${e.target.value}%`);
+})
+
 sr.reveal('.home-text',{delay:200, origin:'top'})
 sr.reveal('.contact-icons',{delay:200, origin:'top'})
+sr.reveal('.about-text',{delay:200, origin:'left'})
+sr.reveal('.img-about',{delay:200, origin:'right'})
