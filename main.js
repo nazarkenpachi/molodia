b64a1be1281d62d838c203847bcd3712cc992bf5
NazarKenpachi <nazar.drozd.ip32@gmail.com>
2024-09-12 01:52:51 +0300

    all sections are made

--- main.js
+++ main.js
@@ -49,6 +49,21 @@ document.querySelector('.slider').addEventListener('input', (e) => {
   foregroundImg.style.setProperty('width', `${e.target.value}%`);
 })
 
+var lastScrollTop = 0;
+var header = document.querySelector('header');
+window.addEventListener("scroll", function(){
+   var scrollTop = window.pageYOffset || document
+      .documentElement.scrollTop;
+   if (scrollTop > lastScrollTop){
+      header.style.top="-80px"
+   } else {
+      header.style.top="0";
+   }
+   lastScrollTop = scrollTop;
+   header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
 sr.reveal('.home-text',{delay:200, origin:'top'})
 sr.reveal('.contact-icons',{delay:200, origin:'top'})
 sr.reveal('.about-text',{delay:200, origin:'left'})
